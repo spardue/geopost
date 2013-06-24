afc319cc6dacd727a6231e51c4d02a42c4b3c0dd
Robert Kernan <rkernan@gmail.com>
2013-06-24 15:26:41 -0400

    Retabbed file.

--- posts_controller.py
+++ posts_controller.py
@@ -7,68 +7,68 @@ from sqlalchemy import desc
 import web
 
 class PostsController:
-  # list all posts
-  def list(self, latitude=None, longitude=None, radius=None):
-    session = Session()
-    if latitude != None and longitude != None and radius != None:
-      # TODO order by Post.created_at
-      print "hi"
-      posts = session.query(Post).filter(earth_dist(Post.latitude, latitude, Post.longitude, longitude) <= radius).order_by(Post.id.desc()).all()
-    else:
-      # TODO order by Post.created_at
-      posts = session.query(Post).order_by(Post.id.desc()).all()
-    session.close()
-    return posts
+    # list all posts
+    def list(self, latitude=None, longitude=None, radius=None):
+        session = Session()
+        if latitude != None and longitude != None and radius != None:
+            # TODO order by Post.created_at
+            print "hi"
+            posts = session.query(Post).filter(earth_dist(Post.latitude, latitude, Post.longitude, longitude) <= radius).order_by(Post.id.desc()).all()
+        else:
+            # TODO order by Post.created_at
+            posts = session.query(Post).order_by(Post.id.desc()).all()
+        session.close()
+        return posts
 
-  # show a single post
-  def show(self, post_id):
-    session = Session()
-    post = session.query(Post).get(post_id)
-    session.close()
-    if post == None:
-      raise web.notfound()
-    else:
-      return post
+    # show a single post
+    def show(self, post_id):
+        session = Session()
+        post = session.query(Post).get(post_id)
+        session.close()
+        if post == None:
+            raise web.notfound()
+        else:
+            return post
 
-  # add a new post
-  def add(self, message, latitude, longitude, radius, time_limit=60):
-    print radius
-    post = Post(message, latitude, longitude, radius, time_limit)
-    session = Session()
-    session.add(post)
-    session.commit()
-    session.close()
-    return post
+    # add a new post
+    def add(self, message, latitude, longitude, radius, time_limit=60):
+        print radius
+        post = Post(message, latitude, longitude, radius, time_limit)
+        session = Session()
+        session.add(post)
+        session.commit()
+        session.close()
+        return post
 
-  # GET /posts
-  # GET /posts/<id>
-  def GET(self, post_id=None):
-    if post_id == None:
-      input = web.input()
-      try:
-	print "@"*55555
-        latitude = input['latitude']
-        longitude = input['longitude']
-        radius = input['radius']
-	print "I AM IN GET DOING GET THINGS YO"
-        return self.list(latitude, longitude, radius)
-      except KeyError:
-        return self.list()
-    else:
-      return self.show(post_id)
+    # GET /posts
+    # GET /posts/<id>
+    def GET(self, post_id=None):
+        if post_id == None:
+            input = web.input()
+            try:
+                print "@"*55555
+                latitude = input['latitude']
+                longitude = input['longitude']
+                radius = input['radius']
+                print "I AM IN GET DOING GET THINGS YO"
+                return self.list(latitude, longitude, radius)
+            except KeyError:
+                return self.list()
+        else:
+            return self.show(post_id)
 
-  # POST /posts
-  def POST(self, post_id=None):
-    if post_id != None:
-      raise web.notfound()
-    else:
-      input = web.input()
-      message = input['message']
-      latitude = input['latitude']
-      longitude = input['longitude']
-      radius = input['radius']
-      try:
-        time_limit = input['time_limit']
-        self.add(message, latitude, longitude, radius, time_limit)
-      except KeyError:
-        self.add(message, latitude, longitude, radius)
+    # POST /posts
+    def POST(self, post_id=None):
+        if post_id != None:
+            raise web.notfound()
+        else:
+            input = web.input()
+            message = input['message']
+            latitude = input['latitude']
+            longitude = input['longitude']
+            radius = input['radius']
+            try:
+                time_limit = input['time_limit']
+                self.add(message, latitude, longitude, radius, time_limit)
+            except KeyError:
+                self.add(message, latitude, longitude, radius)

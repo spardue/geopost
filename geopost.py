0bf8dbea7b13be89d36333c2033b086282281401
Robert Kernan <rkernan@gmail.com>
2013-06-24 14:52:17 -0400

    Converted 2 space indent to 4.

--- geopost.py
+++ geopost.py
@@ -5,16 +5,16 @@ import web
 
 routes = (
     '/posts', 'PostsController',
-	'/posts/([0-9]+)', 'PostsController',
-	'/', 'UiGiver',
+    '/posts/([0-9]+)', 'PostsController',
+    '/', 'UiGiver',
 )
 
 class UiGiver:
-	def GET(self):
-		return web.redirect("/static/ui.html")
+    def GET(self):
+        return web.redirect("/static/ui.html")
 
 app = web.application(routes, globals())
 
 if __name__ == '__main__':
-  Base.metadata.create_all()
-  app.run()
+    Base.metadata.create_all()
+    app.run()
--- post.py
+++ post.py
@@ -6,27 +6,27 @@ import json
 import web
 
 class Post(Base):
-  __tablename__ = 'post'
+    __tablename__ = 'post'
 
-  id = Column(Integer, primary_key=True)
-  created_at = Column(DateTime, default=datetime.utcnow())
-  time_limit = Column(Integer, nullable=False)
-  message = Column(String(256), nullable=False)
-  longitude = Column(Float, nullable=False)
-  latitude = Column(Float, nullable=False)
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow())
+    time_limit = Column(Integer, nullable=False)
+    message = Column(String(256), nullable=False)
+    longitude = Column(Float, nullable=False)
+    latitude = Column(Float, nullable=False)
 
-  def __init__(self, message, latitude, longitude, time_limit=60):
-    self.message = message
-    self.latitude = latitude
-    self.longitude = longitude
-    self.time_limit = time_limit
+    def __init__(self, message, latitude, longitude, time_limit=60):
+        self.message = message
+        self.latitude = latitude
+        self.longitude = longitude
+        self.time_limit = time_limit
 
-  def __repr__(self):
-    return json.dumps({
-        "id": self.id,
-        "created_at": self.created_at.isoformat(),
-        "time_limit": self.time_limit,
-        "message": self.message,
-        "longitude": self.longitude,
-        "latitude": self.latitude
-      })
+    def __repr__(self):
+        return json.dumps({
+                "id": self.id,
+                "created_at": self.created_at.isoformat(),
+                "time_limit": self.time_limit,
+                "message": self.message,
+                "longitude": self.longitude,
+                "latitude": self.latitude
+            })

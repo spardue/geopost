9d0d86e59da073533fefe19ba5f96a83f023347c
Robert Kernan <rkernan@gmail.com>
2013-06-30 17:20:57 -0400

    Switched from sqlite to mysql.

--- db.py
+++ db.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///geopost.db', echo=True)
+# engine = create_engine('sqlite:///geopost.db', echo=True)
+engine = create_engine('mysql://geopost:post@localhost', echo=True)
+engine.execute("CREATE DATABASE IF NOT EXISTS geopost")
+engine.execute("USE geopost")
 
 Base = declarative_base(bind=engine)
 Session = scoped_session(sessionmaker(engine))
